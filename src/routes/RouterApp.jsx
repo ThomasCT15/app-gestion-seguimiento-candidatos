9fcc42fdb802c50a90fac4acd35f1ee5048b3e44
thomasct15 <thomas.ci152003@gmail.com>
2025-11-07 12:47:18 -0500

    Listado de postulaciones

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -2,6 +2,9 @@ import Login from "../pages/auth/login/Login";
 import Registro from "../pages/auth/registro/Registro";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Home from "../pages/home/Home";
+import Candidatos from "../pages/candidatos/Candidatos"
+import Vacantes from "../pages/vacantes/Vacantes"
+import Postulaciones from "../pages/postulaciones/Postulaciones"
 import RutaProtegida from "../components/RutaProtegida";
 
 export let RouterApp = [
@@ -20,5 +23,17 @@ export let RouterApp = [
     {
         path: "/",
         element: <Home />,
+    },
+    {
+        path: "/candidatos",
+        element: <RutaProtegida componente={<Candidatos />} />,
+    },
+    {
+        path: "/vacantes",
+        element: <RutaProtegida componente={<Vacantes />} />,
+    },
+    {
+        path: "/postulaciones",
+        element: <RutaProtegida componente={<Postulaciones />} />,
     },
 ];
